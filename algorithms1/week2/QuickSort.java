f375dc6c6facb032cf38afd4c48c057ef68f202d
Matt Chowning <mchowning@gmail.com>
2015-02-01 08:24:49 -0500

    Updated to work better with IntelliJ

--- algorithms1/week2/QuickSort.java
+++ algorithms1/week2/QuickSort.java
@@ -1,10 +1,11 @@
-import java.util.Arrays;
-import java.util.List;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.nio.charset.StandardCharsets;
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
+@SuppressWarnings("WrongPackageStatement")
 public class QuickSort {
 
     private static int numComparisons;
